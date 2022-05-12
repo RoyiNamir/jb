02b760a638d89ba26aefaa8a292b712ce4b37aa4
unknown <namir.78@gmail.com>
2022-05-12 20:30:18 +0300

    4

--- 12-5-2022/es8-samples.js
+++ 12-5-2022/es8-samples.js
@@ -2,8 +2,59 @@
 let o = { name: "avi", age: 42, dob: new Date(1978, 11, 22) }
 
  
-console.log(Object.values(o));
-console.log(Object.keys(o));
-console.log(Object.entries(o));
+//console.log(Object.values(o));
+//console.log(Object.keys(o));
+//console.log(Object.entries(o));
+
+ function doSomething( a,b,...other) {
+     console.log(a);
+     console.log(b);
+     console.log(other);
+ }
+//doSomething(5,6,7,8,9)
+
+//array flat flatmap
+
+let arr = [4,5,6,7,8,['🎈','🎉' , [9,9,9,[0,0,0]]]]
+
+ 
+
+//console.log(arr);
+//let flattedArray=arr.flat(Number.MAX_VALUE);
+//console.log(flattedArray);
+//let flattedMappedArray=arr.flatMap(d=>d+"!",);
+ //console.log(flattedMappedArray);
+
+
+function aaa(a,b,g){
+ console.log(1);
+ //blabla
+ console.log(2);
+ console.log('avi');
+}
+
+//console.log("the function aaa accepts X parameters , which is   = "+aaa.length);
+//console.log("does the aaa function contain the word 'bla'  ? " + aaa.toString().includes("avi"));
+
+
+
+class Person{
+ #food = "milk"
+ eat () {console.log("eating " + this.#food);}
+
+}
+
+let p = new Person();
+p.eat();
+console.log(p.food);
+
+
+
+
+
+
+
+
+
 
 
--- 12-5-2022/promisesAdvanced/1.js
+++ 12-5-2022/promisesAdvanced/1.js
@@ -1,16 +1,16 @@
-function createPromise(value, time) {
+function createPromise(value, time, isResolved) {
     return new Promise((resolve, reject) => {
-        setTimeout(() => resolve(value), time)
+        setTimeout(() => isResolved ? resolve(value) : reject(value), time)
     })
 }
 
 
 async function doSomethingAsyncWhenAllReolved() {
-    let p1 = createPromise("hebrew", 2000)
-    let p2 = createPromise("english", 4000)
-    let p3 = createPromise("india", 300)
+    let p1 = createPromise("hebrew", 2000, true)
+    let p2 = createPromise("english", 4000, true)
+    let p3 = createPromise("india", 300, true)
 
-    let result = await Promise.all([p1, p2, p3])
+    let result = await Promise.all([p1, p2, p3]);
 
     let sel = document.getElementById("sel");
     result.forEach(element => {
@@ -20,26 +20,31 @@ async function doSomethingAsyncWhenAllReolved() {
         sel.appendChild(el);
     });
 
-  
+
 }
 
 
 async function doSomethingAsyncWhenAnyReolved() {
-    let p1 = createPromise("lighter from avi", 1000)
-    let p2 = createPromise("lighter from eli", 6000)
-    let p3 = createPromise("lighter from gali", 5000)
+    let p1 = createPromise("lighter from avi", 1000, false)
+    let p2 = createPromise("lighter from eli", 6000, true)
+    let p3 = createPromise("lighter from gali", 5000, false)
 
-    let singleValue = await Promise.any([p1, p2, p3])
-
-    let sel = document.getElementById("sel");
-    
-        var el = document.createElement("option");
-        el.textContent = singleValue;
-        el.value = singleValue;
-        sel.appendChild(el);
+    let singleValue = Promise.any([p1, p2, p3])
+                             .then(f => console.log(f))
+                             .finally(() => console.log("operation has finished"))
+    //console.log(singleValue);
+    /*  let sel = document.getElementById("sel");
       
+          var el = document.createElement("option");
+          el.textContent = singleValue;
+          el.value = singleValue;
+          sel.appendChild(el);*/
+
 }
 
 
 //doSomethingAsyncWhenAllReolved();
 doSomethingAsyncWhenAnyReolved();
+
+
+//see also : https://jsbin.com/sutuyaximi/1/edit?js,console,output
