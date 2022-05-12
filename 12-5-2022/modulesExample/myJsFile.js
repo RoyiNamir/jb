cbfc804cc36804964ac85c3e1fbd0844231d1f57
unknown <namir.78@gmail.com>
2022-05-12 17:49:44 +0300

    f

---
+++ 12-5-2022/modulesExample/myJsFile.js
@@ -0,0 +1,8 @@
+import {add} from "./math.js" //for this to work , please add the package.json
+
+console.log(add(4,5));
+
+
+//for browser demo  ( non-node) please visit : https://stackblitz.com/edit/js-efj1bd?file=index.js,math.js
+
+
